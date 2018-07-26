8a37f6071e6df8de965c0a576b30ebeda1f3e4d2
JagtapVishal <vishal.jagtap@asentech.com>
2018-07-26 17:25:18 +0530

    commit TestBuild.java

--- my-app/src/main/java/webapp/TestBuild.java
+++ my-app/src/main/java/webapp/TestBuild.java
@@ -9,24 +9,27 @@ import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import utility.Constant;
 
-public class TestBuild {
+
+public class TestBuild extends Constant {
 	
 	protected WebDriver driver;
 	
 	
 	public void setup(){
 		
+		
 		BasicConfigurator.configure();
 		PropertyConfigurator.configure("log4j.properties");
 		Logger log = Logger.getLogger(login_page.class.getClass());
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\Haymarket_Project\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", driver_path);
 		driver=new ChromeDriver();
 		log.info("Browser is Opened");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("http://35.173.239.167/login-standalone/");
+		driver.get(haymarket_QA_url);
 		//System.out.println(".........geturl");
 		log.info("Site is Opened");
 		
