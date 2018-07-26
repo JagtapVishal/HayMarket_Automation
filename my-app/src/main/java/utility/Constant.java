0d42ab3410d99fbe432352e29f21bc38baefc4f0
JagtapVishal <vishal.jagtap@asentech.com>
2018-07-26 17:24:10 +0530

    Commit constant.java

--- my-app/src/main/java/utility/Constant.java
+++ my-app/src/main/java/utility/Constant.java
@@ -3,8 +3,10 @@ package utility;
 public class Constant {
 	 
     
-    public static final String path = "D://Haymarket_Project//my-app//TestData//TestData.xlsx";
+    public static final String path = "E:\\Git\\HayMarket_Automation\\my-app\\TestData\\TestData.xlsx";
     
+    public static final String driver_path = "E:\\Git\\HayMarket_Automation\\chromedriver_win32\\chromedriver.exe";
     
+    public static final String haymarket_QA_url = "http://35.173.239.167/login-standalone/";
     
  }
